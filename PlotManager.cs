e1ebe9849bfbcae4afb4babfec3a33e082476cef
silverlybee <83087373+silverlybee@users.noreply.github.com>
2021-04-23 10:44:54 -0700

    Part 1 - Planting and Harvesting

---
+++ PlotManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotManager : MonoBehaviour
+{
+
+    bool isPlanted = false;
+    SpriteRenderer plant;
+    BoxCollider2D plantCollider;
+
+    public Sprite[] plantStages;
+    int plantStage = 0;
+    float timeBtwStages = 2f;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plant = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        plantCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlanted)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0 && plantStage<plantStages.Length-1)
+            {
+                timer = timeBtwStages;
+                plantStage++;
+                UpdatePlant();
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlanted)
+        {
+            if (plantStage == plantStages.Length - 1)
+            {
+                Harvest();
+            }
+        }
+        else
+        {
+            Plant();
+        }
+
+    }
+
+    void Harvest()
+    {
+        isPlanted = false;
+        plant.gameObject.SetActive(false);
+    }
+
+    void Plant()
+    {
+        isPlanted = true;
+        plantStage = 0;
+        UpdatePlant();
+        timer = timeBtwStages;
+        plant.gameObject.SetActive(true);
+    }
+
+    void UpdatePlant()
+    {
+        plant.sprite = plantStages[plantStage];
+        plantCollider.size = plant.sprite.bounds.size;
+        plantCollider.offset = new Vector2(0,plant.bounds.size.y/2);
+    }
+
+}
