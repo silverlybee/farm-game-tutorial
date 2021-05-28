e302ff443ff3bbb744e4a40cd7ab25b528266799
silverlybee <83087373+silverlybee@users.noreply.github.com>
2021-05-27 17:44:28 -0700

    Add files via upload

---
+++ StoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreManager : MonoBehaviour
+{
+    public GameObject plantItem;
+    List<PlantObject> plantObjects = new List<PlantObject>();
+    private void Awake()
+    {
+        //Assets/Resources/Plants
+        var loadPlants = Resources.LoadAll("Plants", typeof(PlantObject));
+        foreach (var plant in loadPlants)
+        {
+            plantObjects.Add((PlantObject)plant);
+        }
+        plantObjects.Sort(SortByPrice);
+
+        foreach (var plant in plantObjects)
+        {
+            PlantItem newPlant = Instantiate(plantItem, transform).GetComponent<PlantItem>();
+            newPlant.plant = plant;
+        }
+    }
+
+    int SortByPrice(PlantObject plantObject1, PlantObject plantObject2)
+    {
+        return plantObject1.buyPrice.CompareTo(plantObject2.buyPrice);
+    }
+
+    int SortByTime(PlantObject plantObject1, PlantObject plantObject2)
+    {
+        return plantObject1.timeBtwStages.CompareTo(plantObject2.timeBtwStages);
+    }
+}
