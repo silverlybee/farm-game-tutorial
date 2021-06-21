95c3b7e352628092685fe583460e5b7233f32700
Silverly Bee <83087373+silverlybee@users.noreply.github.com>
2021-06-20 18:21:36 -0700

    Watering, Fertilizing & Buying Plots

--- PlotManager.cs
+++ PlotManager.cs
@@ -22,6 +22,14 @@ public class PlotManager : MonoBehaviour
 
     FarmManager fm;
 
+    bool isDry = true;
+    public Sprite drySprite;
+    public Sprite normalSprite;
+    public Sprite unavailableSprite;
+
+    float speed=1f;
+    public bool isBought=true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,14 +37,23 @@ public class PlotManager : MonoBehaviour
         plantCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
         fm = transform.parent.GetComponent<FarmManager>();
         plot = GetComponent<SpriteRenderer>();
+        if (isBought)
+        {
+            plot.sprite = drySprite;
+        }
+        else
+        {
+            plot.sprite = unavailableSprite;
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPlanted)
+        if (isPlanted && !isDry)
         {
-            timer -= Time.deltaTime;
+            timer -= speed*Time.deltaTime;
 
             if (timer < 0 && plantStage<selectedPlant.plantStages.Length-1)
             {
@@ -51,23 +68,52 @@ public class PlotManager : MonoBehaviour
     {
         if (isPlanted)
         {
-            if (plantStage == selectedPlant.plantStages.Length - 1 && !fm.isPlanting)
+            if (plantStage == selectedPlant.plantStages.Length - 1 && !fm.isPlanting && !fm.isSelecting)
             {
                 Harvest();
             }
         }
-        else if(fm.isPlanting && fm.selectPlant.plant.buyPrice <= fm.money)
+        else if(fm.isPlanting && fm.selectPlant.plant.buyPrice <= fm.money && isBought)
         {
             Plant(fm.selectPlant.plant);
         }
-
+        if (fm.isSelecting)
+        {
+            switch (fm.selectedTool)
+            {
+                case 1:
+                    if (isBought) {
+                        isDry = false;
+                        plot.sprite = normalSprite;
+                        if (isPlanted) UpdatePlant();
+                    }
+                    break;
+                case 2:
+                    if (fm.money>=10 && isBought)
+                    {
+                        fm.Transaction(-10);
+                        if (speed < 2) speed += .2f;
+                    }
+                    break;
+                case 3:
+                    if (fm.money >= 100 && !isBought)
+                    {
+                        fm.Transaction(-100);
+                        isBought = true;
+                        plot.sprite = drySprite;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     private void OnMouseOver()
     {
         if (fm.isPlanting)
         {
-            if(isPlanted || fm.selectPlant.plant.buyPrice > fm.money)
+            if(isPlanted || fm.selectPlant.plant.buyPrice > fm.money || !isBought)
             {
                 //can't buy
                 plot.color = unavailableColor;
@@ -76,6 +122,36 @@ public class PlotManager : MonoBehaviour
             {
                 //can buy
                 plot.color = availableColor;
+            }
+        }
+        if (fm.isSelecting)
+        {
+            switch (fm.selectedTool)
+            {
+                case 1:
+                case 2:
+                    if (isBought && fm.money>=(fm.selectedTool-1)*10)
+                    {
+                        plot.color = availableColor;
+                    }
+                    else
+                    {
+                        plot.color = unavailableColor;
+                    }
+                    break;
+                case 3:
+                    if (!isBought && fm.money>=100)
+                    {
+                        plot.color = availableColor;
+                    }
+                    else
+                    {
+                        plot.color = unavailableColor;
+                    }
+                    break;
+                default:
+                    plot.color = unavailableColor;
+                    break;
             }
         }
     }
@@ -91,6 +167,9 @@ public class PlotManager : MonoBehaviour
         isPlanted = false;
         plant.gameObject.SetActive(false);
         fm.Transaction(selectedPlant.sellPrice);
+        isDry = true;
+        plot.sprite = drySprite;
+        speed = 1f;
     }
 
     void Plant(PlantObject newPlant)
@@ -108,7 +187,14 @@ public class PlotManager : MonoBehaviour
 
     void UpdatePlant()
     {
-        plant.sprite = selectedPlant.plantStages[plantStage];
+        if (isDry)
+        {
+            plant.sprite = selectedPlant.dryPlanted;
+        }
+        else
+        {
+            plant.sprite = selectedPlant.plantStages[plantStage];
+        }
         plantCollider.size = plant.sprite.bounds.size;
         plantCollider.offset = new Vector2(0,plant.bounds.size.y/2);
     }
